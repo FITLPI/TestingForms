6aace522c528dc9d11afe582648a179955b864bf
FITLPI <boykov2525@gmail.com>
2023-10-24 19:52:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TestingForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonView = new System.Windows.Forms.Button();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.labelView = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(12, 96);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(118, 66);
+            this.buttonView.TabIndex = 0;
+            this.buttonView.Text = "button1";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(12, 68);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(118, 22);
+            this.textBoxC.TabIndex = 2;
+            // 
+            // labelView
+            // 
+            this.labelView.AutoSize = true;
+            this.labelView.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelView.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.labelView.Location = new System.Drawing.Point(145, 12);
+            this.labelView.Name = "labelView";
+            this.labelView.Size = new System.Drawing.Size(214, 29);
+            this.labelView.TabIndex = 4;
+            this.labelView.Text = "Ваш треугольник:";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(12, 40);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(118, 22);
+            this.textBoxB.TabIndex = 5;
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(12, 12);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(118, 22);
+            this.textBoxA.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 450);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.labelView);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.buttonView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label labelView;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxA;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static bool CheckInt(string value)
+        {
+            int result;
+                
+            if (!int.TryParse(value, out result) || result <= 0)
+            {
+                MessageBox.Show("Данное значение не может быть стороной треугольника");
+                return false;
+            }
+            return true;
+
+        }
+
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            int a, b , c;
+
+            if (CheckInt(textBoxA.Text) && CheckInt(textBoxB.Text) && CheckInt(textBoxC.Text))
+            {
+                a = int.Parse(textBoxA.Text);
+                b = int.Parse(textBoxB.Text);
+                c = int.Parse(textBoxC.Text);
+
+                if (a + b <= c || c + b <= a || a + c <= b) labelView.Text = "Ваш треугольник:\nне существует";
+                else if (a == b && a == c) labelView.Text = "Ваш треугольник:\nровносторонний";
+                else if (b == c && b != a || a == c && b != a || b == a && c != a) labelView.Text = "Ваш треугольник:\nравнобедренный";
+                else if (a != b && b != c && c != a) labelView.Text = "Ваш треугольник:\nразносторонний";
+            }
+        }
+    }
+}
